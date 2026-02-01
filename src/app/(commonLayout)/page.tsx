37ca0c9e5201927c12cb0d950afbed482dde8de2
Rubaid07 <rubaidindu@gmail.com>
2026-02-01 11:44:59 +0600

    added proxy file to protect private route

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,7 +1,5 @@
 import { Button } from "@/components/ui/button";
-
 export default async function Home() {
-  const res = await fetch("http://localhost:5000/api/auth/get-session");
   return (
     <div className="m-3">
       <Button>Click Here</Button>
