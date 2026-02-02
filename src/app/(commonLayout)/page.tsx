839ccfe82ccb84bc8bd9ec159a1b9955cac80a02
Rubaid07 <rubaidindu@gmail.com>
2026-02-02 18:52:27 +0600

    implement ISR

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,8 +1,17 @@
-import { Button } from "@/components/ui/button";
+import BlogCard from "@/components/modules/homepage/BlogCard";
+import { blogService } from "@/services/blog.service";
+import { BlogPost } from "@/types";
 export default async function Home() {
+  const { data } = await blogService.getBlogPosts({
+    isFeatured: false,
+  }, {
+    cache: "no-store"
+  });
   return (
-    <div className="m-3">
-      <Button>Click Here</Button>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto px-4 gap-5">
+      {data?.data?.map((post: BlogPost) => (
+        <BlogCard key={post.id} post={post} />
+      ))}
     </div>
   );
 }
